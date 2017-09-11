f7373de25f1af3a7bc50536aaf5c04b6fe97c654
Dan Kiss and Qudsia Ahmad <dan@>
2017-09-11 12:02:54 -0700

    fix triangle logic.rb

---
+++ lib/triangle_logic.rb
@@ -0,0 +1,24 @@
+class Triangle
+  def initialize(input_1, input_2, input_3)
+    @input_1 = input_1.to_i
+    @iput_2 = input_2.to_i
+    @input_3 = input_3.to_i
+  end
+
+  def triangle_method()
+    puts "Enter in your triangle. Note: side 1 must be the shortest side, side 2 must be the middle length and side 3 must be the longest, if all all sides, the side 3 must be the longest, if all are equal just type it in"
+    if (@input_1 + @input2 <= @input_3)
+      puts "This is not a triangle"
+    elsif (@input_1 == @input_2) & (@input_2 == @input_3)
+      puts "This is an equilateral triangle."
+    elsif (@input_1 == @input_2) & (@input_2 != @input3)
+      # (@input_1 == @input_3) || (@input_1 == @input_2) && (@input_2 == @input_3 )
+      puts "This is an isoceles triangle."
+    elsif (@input_1 != @input_2) & (@input_2 != @input_3) & (@input_1 != @input_3)
+      puts "This is a scalene triangle."
+  end
+
+
+
+
+end
