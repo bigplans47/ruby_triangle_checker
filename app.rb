d819edef3be8dff049e9b6f081e10844022ac877
Dan Kiss and Qudsia Ahmad <qudsia@>
2017-09-11 13:47:29 -0700

    test methods

---
+++ app.rb
@@ -0,0 +1,17 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('./lib/**/*.rb')
+require('./lib/triangle_logic.rb')
+
+get('/') do
+    erb(:input)
+end
+
+get('/output') do
+  @input_1 = params.fetch('input_1')
+  @input_2 = params.fetch('input_2')
+  @input_3 = params.fetch('input_2')
+  new_triangle = Triangle.new(@input_1, @input_2, @input_3)
+  @output_result = new_triangle.triangle_method()
+  erb(:output)
+end
--- lib/triangle_logic.rb
+++ lib/triangle_logic.rb
@@ -17,8 +17,26 @@ class Triangle
     elsif (@input_1 != @input_2) & (@input_2 != @input_3) & (@input_1 != @input_3)
       puts "This is a scalene triangle."
   end
-
-
-
-
 end
+
+# def initialize(input_1raw, input_2raw, input_3raw)
+#   array = [input_1raw.to_i, input_2raw.to_i, input_3raw.to_i]
+#   @array = array.sort!
+#   @input_1 = @array[0]
+#   @input_2 = @array[1]
+#   @input_3 = @array[2]
+#   return
+# end
+#
+# def triangle_method()
+#   if (@input_1 + @input2 <= @input_3)
+#     puts "This is not a triangle"
+#   elsif (@input_1 == @input_2) & (@input_2 == @input_3)
+#     puts "This is an equilateral triangle."
+#   elsif (@input_1 == @input_2) & (@input_2 != @input3)
+#     # (@input_1 == @input_3) || (@input_1 == @input_2) && (@input_2 == @input_3 )
+#     puts "This is an isoceles triangle."
+#   elsif (@input_1 != @input_2) & (@input_2 != @input_3) & (@input_1 != @input_3)
+#     puts "This is a scalene triangle."
+#   end
+# end
